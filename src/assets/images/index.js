884bcea4d03f09a0c140177de09a461e41d00365
zl8115 <zl8115@ic.ac.uk>
2020-09-24 18:47:50 +0100

    File extension fixes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,4 @@
 export const mainPic = require('./mainPic.JPG');
-export const profilePic = require('./profilePic.jpg');
+export const profilePic = require('./profilePic.JPG');
 export const ZeLogo = require('./ZeLogo.png');
 export const HelloThere = require('./404.jpg');
