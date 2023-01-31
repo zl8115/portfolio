ab40eff64f05644f11b32e3c89ab88636f19bc66
zl8115 <zl8115@ic.ac.uk>
2023-01-31 00:08:40 +0000

    Use fontsource-roboto instead of typefont-roboto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'typeface-roboto';
+import 'fontsource-roboto/latin.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
